08903f5ce1bb9a4aadebc4ff968a595e1192aca5
Albert Ventura <albert@venturads.com>
2017-05-18 04:11:48 +0000

    fix missing image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "800x800>", thumb: "100x100>" }, default_url: "/images/normal/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :title, :presence => true, :length => { :minimum => 5}
   validates :description, :presence => true
