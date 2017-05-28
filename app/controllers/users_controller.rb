0c173cb71ed0818060d5fa4b3622d316ec00e92f
Albert Ventura <albert@venturads.com>
2017-05-28 18:34:43 +0000

    update login, fix footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    before_filter :authenticate_user!
+    before_filter :authenticate_user!, :except => [:about]
     def show
         @user = User.find(params[:id])
     end
+    
 end
